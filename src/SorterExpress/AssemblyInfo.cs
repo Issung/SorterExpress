cb1142bf74222f33ed81576ff214ead19013849d
Issung <joel@sonofgoran.com>
2024-03-16 11:27:01 +1100

    Fix Windows support warnings across project

--- src/SorterExpress/AssemblyInfo.cs
+++ src/SorterExpress/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using System.Runtime.Versioning;
 
 [assembly: AssemblyVersion("0.3.0")]
 [assembly: AssemblyFileVersion("0.3.0")]
+[assembly: SupportedOSPlatform("windows")]
